75432fce96cc872c0ea87bc58fe6bb92c3156c9d
Oujaber Ousama <oujaberousama@gmail.com>
2025-10-28 09:15:00 +0100

    Initial project setup with Spring Boot and Maven configuration

---
+++ src/main/java/com/protocol/supplychainx/SupplyChainXApplication.java
@@ -0,0 +1,15 @@
+package com.protocol.supplychainx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SupplyChainXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplyChainXApplication.class, args);
+    }
+
+}
